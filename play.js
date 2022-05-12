b867316bd908e726b45d5bd7607f898b93d88e6c
lorleod <taylor.mcleod@gmail.com>
2022-05-12 00:18:17 +0000

    added setupInput and handleuser input to listen for input

--- play.js
+++ play.js
@@ -1,5 +1,21 @@
 const net = require("net");
 const { connect } = require("./client");
 
+const handleUserInput = function () {
+  if (key === '\u0003') {
+    process.exit();
+  }
+}
+
+// setup interface to handle user input from stdin
+const setupInput = function () {
+  const stdin = process.stdin;
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handUserInput);
+  return stdin;
+};
+
 console.log("Connecting ...");
 connect();
